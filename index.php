4189c87f15e443385ac21fd7ec4ae9fa7022f76f
Mathala Vamsi <66720568+vamsi-01@users.noreply.github.com>
2020-07-15 00:09:33 +0530

    commit

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <body bgcolor='aqua'>
 <pre>
 <?php
-print "My Webserver Launched!!";
+print "Welecome to my Page of AWS Task 2.";
+print "\n";
 
+print "My Webserver Launched!!";
 print "\n";
 
 print `free -m`;
