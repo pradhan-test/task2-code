94c3562244bdc72cfc0116b4b168d5a7f90ad8c4
Mathala Vamsi <66720568+vamsi-01@users.noreply.github.com>
2020-07-15 15:04:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <body bgcolor='aqua'>
 <pre>
 <?php
+print "                    ";
 print "Welecome to my Page of AWS Task 2.";
 print "\n";
 
+print "                    ";
 print "My Webserver Launched!!";
 print "\n";
 
+print "                    ";
 print `free -m`;
 
+print "                    ";
 print `ifconfig`;
 
+print "                    ";
 print `date`;
 
 ?>
